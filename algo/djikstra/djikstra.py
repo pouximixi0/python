37d982c85be88505ec02f3078b3a156248256e1a
pouximixi <pouximixi@gmail.com>
2026-09-21 15:41:13 +0200

    base algo dijkstra

--- algo/djikstra/djikstra.py
+++ algo/djikstra/djikstra.py
@@ -1,9 +1,9 @@
-import graphviz
+#import graphviz
 import os
 os.environ["PATH"] += os.pathsep + r'C:\Program Files\Graphviz\bin'
 
 depart = ""
-dot = graphviz.Digraph(comment='Graph')
+#dot = graphviz.Digraph(comment='Graph')
 
 graph = {
   "Paris": {"Rennes": 1, "Tours": 1, "Lille": 1, "Nancy": 1},
@@ -23,17 +23,69 @@ graph = {
 
 for i in graph:
   for j in graph[i]:
-    dot.edge(i,j, label=str(graph[i][j]))
+    #dot.edge(i,j, label=str(graph[i][j]))
+    pass
   
 
-dot.render('graph_output', format='png', cleanup=True)
+#dot.render('graph_output', format='png', cleanup=True)
 
+def djikstra(graph, start):
+  distance = {}
+  for i in graph:
+    distance[i] = float('inf')
+    distance[start] = 0
+    
+  for i in graph:
+    lowest = float('inf')
+    lowest_node = None
+    if i == start:
+      for j in graph[i]:
+        if float(graph[i][j]) < lowest:
+          lowest = float(graph[i][j])
+          lowest_node = j
+      distance[lowest_node] = graph[i][lowest_node]
+  print(distance)
+  
 class Graph:
   def __init__(self, graph: dict = {}):
     self.graph = graph
     
   def add_edge(self, node1, node2, weight):
+    if node1 not in self.graph:
+      self.graph[node1] = {}
     self.graph[node1][node2] = weight
     
-Graph.add_edge("A", "B", 5)
+    if node2 not in self.graph:
+      self.graph[node2] = {}
+    self.graph[node2][node1] = weight
     
+G = Graph()
+G.add_edge("A", "B", 16)
+G.add_edge("B", "E", 5)
+G.add_edge("E", "F", 2)
+G.add_edge("F", "D", 12)
+G.add_edge("D", "B", 4)
+G.add_edge("D", "C", 30)
+
+
+djikstra(G.graph, 'B')
+
+
+"""
+flowchart LR
+
+A((A))
+B((B))
+C((C))
+D((D))
+E((E))
+F((F))
+
+
+A-.16.-B
+B-.5.-E
+E-.2.-F
+F-.12.-D
+D-.4.-B
+D-.30.-C
+"""
